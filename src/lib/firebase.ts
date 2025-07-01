4a16509955684914aee2900f5af0479496863be2
Thimira Vishwa <vishwathimira2@gmail.com>
2025-07-01 16:41:29 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// IMPORTANT: Create a .env.local file in the root of your project
-// and add your Firebase configuration there.
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyARzGUQpki7TrzmlSZSoSS4FuwcOyWNoEc",
+  authDomain: "aarya-bathware.firebaseapp.com",
+  projectId: "aarya-bathware",
+  storageBucket: "aarya-bathware.firebasestorage.app",
+  messagingSenderId: "480140706464",
+  appId: "1:480140706464:web:5ca9db86c04023743d3fc5"
 };
 
 // Initialize Firebase
