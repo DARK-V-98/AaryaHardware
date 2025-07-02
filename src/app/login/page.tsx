44c6a79354ecba896cf5e73c9da9e900ef4a52a9
Thimira Vishwa <vishwathimira2@gmail.com>
2025-07-02 08:50:22 +0000

    no login and signup are not responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -51,7 +51,7 @@ export default function LoginPage() {
 
   return (
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
+      <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="mx-auto grid w-full max-w-[350px] gap-6">
            <div className="grid gap-2 text-center">
              <Link href="/" className="flex items-center justify-center gap-2 font-bold text-2xl font-headline">
