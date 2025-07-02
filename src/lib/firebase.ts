bf09ad38a2a0789aeb8294e170fca412ad1e5fb8
Thimira Vishwa <vishwathimira2@gmail.com>
2025-07-02 04:15:11 +0000

    Error fetching featured products:  FirebaseError: [code=failed-precondit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyARzGUQpki7TrzmlSZSoSS4FuwcOyWNoEc",
   authDomain: "aarya-bathware.firebaseapp.com",
   projectId: "aarya-bathware",
-  storageBucket: "aarya-bathware.appspot.com",
+  storageBucket: "aarya-bathware.firebasestorage.app",
   messagingSenderId: "480140706464",
   appId: "1:480140706464:web:5ca9db86c04023743d3fc5"
 };
